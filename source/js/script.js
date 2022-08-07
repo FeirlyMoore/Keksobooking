1676c0e15fae8ca929bd2e883268892db429b55c
REDFOX39 <daniil.fox1998@yandex.ru>
2022-08-07 03:37:34 +0300

    Dev 0.1 >> Task 1 - part 0.2

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
 function GetRandomNumber(min, max, fixedPointCount) {
+  if (min < 0 || max < 0) return 0;
+
+  if (max < min) [min, max] = [max, min];
+
   let randomNumber = Math.random() * (max - min) + min;
 
   return randomNumber.toFixed(fixedPointCount);
